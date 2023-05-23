010cc163303fbc693d64e5a233e4ce1891064f82
GPTBlur <64308138+JacobMayer@users.noreply.github.com>
2023-05-22 18:09:50 -0700

    Create graph.tsx

---
+++ graph.tsx
@@ -0,0 +1,75 @@
+import { makeScene2D } from "@motion-canvas/2d/lib/scenes";
+import { all, waitFor } from "@motion-canvas/core/lib/flow";
+import { Graph } from '../../components/graph-component/graph';
+
+export default makeScene2D(function* (view) {
+const graph = new Graph({
+  nodes: [
+    // Input Layer
+    [100, 50],  // Node 0
+    [100, 150], // Node 1
+    [100, 250], // Node 2
+    [100, 350], // Node 3
+    [100, 450], // Node 4
+
+    // Hidden Layer
+    [300, 100],  // Node 5
+    [300, 200], // Node 6
+    [300, 300], // Node 7
+    [300, 400], // Node 8
+    [300, 500], // Node 9
+],
+      connections: [
+        [0, 5], // Node 0 -> Node 5
+        [0, 6], // Node 0 -> Node 6
+        [0, 7], // Node 0 -> Node 7
+        [0, 8], // Node 0 -> Node 8
+        [0, 9], // Node 0 -> Node 9
+
+        [1, 5], // Node 1 -> Node 5
+        [1, 6], // Node 1 -> Node 6
+        [1, 7], // Node 1 -> Node 7
+        [1, 8], // Node 1 -> Node 8
+        [1, 9], // Node 1 -> Node 9
+
+        [2, 5], // Node 2 -> Node 5
+        [2, 6], // Node 2 -> Node 6
+        [2, 7], // Node 2 -> Node 7
+        [2, 8], // Node 2 -> Node 8
+        [2, 9], // Node 2 -> Node 9
+
+        [3, 5], // Node 3 -> Node 5
+        [3, 6], // Node 3 -> Node 6
+        [3, 7], // Node 3 -> Node 7
+        [3, 8], // Node 3 -> Node 8
+        [3, 9], // Node 3 -> Node 9
+
+        [4, 5], // Node 4 -> Node 5
+        [4, 6], // Node 4 -> Node 6
+        [4, 7], // Node 4 -> Node 7
+        [4, 8], // Node 4 -> Node 8
+        [4, 9], // Node 4 -> Node 9
+]
+        });
+        
+        view.add(graph);
+        // Highlight node 0 with color 'yellow' for 1 second
+        yield* graph.highlightNode(0, "yellow", 1);
+        // Highlight connection 1 with color 'red' for 1 second
+        yield* graph.highlightConnection(1, "red", 1);
+        
+view.add(graph);
+
+// Highlight connection 1 with color 'red' for 1 second
+yield* graph.highlightConnection(1 , "red", 1);
+// Highlight connection 18 with color 'red' for 1 second
+yield* graph.highlightConnection(18 , "red", 1);
+// Highlight node 0 with color 'yellow' for 1 second
+yield* graph.highlightNode(0, "purple", 1);
+// Change the size of node 2 to 128 for 1 second
+//yield* graph.changeNodeSize(2, 128, 1);
+// move node 1 and connections follow, to position 300, 0 over 3 seconds
+yield* graph.moveNode(1, [300, 0], 3);
+// Highlight node 3 with color 'orange' for 2 seconds
+yield* graph.highlightNode(3, "orange", 2);
+});
